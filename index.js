4744f28e7e4b1d01eb594f1aa2c11e3615bd79ca
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-06-12 18:26:56 -0400

    added questions

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
-// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "Project title:",
+    name: "Title",
+  },
+  {
+    type: "input",
+    message: "Description:",
+    name: "Description",
+  },
+  {
+    type: "input",
+    message: "Installation instructions:",
+    name: "Installation",
+  },
+  {
+    type: "input",
+    message: "Usage information:",
+    name: "Usage",
+  },
+  {
+    type: "input",
+    message: "Contribution guidelines:",
+    name: "Contributing",
+  },
+  {
+    type: "input",
+    message: "Test instructions:",
+    name: "Tests",
+  },
+  {
+    type: "checkbox",
+    message: "What license do you want to use?",
+    choices: ["MIT License", "ISC", "None"],
+    name: "License",
+  },
+  {
+    type: "input",
+    message: "Enter your GitHub username:",
+    name: "GitHub",
+  },
+  {
+    type: "input",
+    message: "Enter your email:",
+    name: "Email",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
