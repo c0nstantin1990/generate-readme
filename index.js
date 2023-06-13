80c8266977333629db7731c1a9a93e088ad5bd42
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-06-12 20:46:37 -0400

    created writetofile function

--- index.js
+++ index.js
@@ -52,7 +52,15 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("README file created successfully!");
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
